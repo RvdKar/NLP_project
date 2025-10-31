ce11be1daed2bdd3725b7e99965bbbb7e06ac5d5
zazizo31 <utkudinc123@gmail.com>
2025-10-31 14:07:06 +0100

    editted topic_modelling.py
    added counter method to count the most occurring themes

--- src/theme_generation_basic.py
+++ src/theme_generation_basic.py
@@ -5,13 +5,12 @@ from preprocessing import load_json
 '''Since it is impossible automatically generate themes using Regular Expressions(RE), keywords will be manually selected'''
 
 themes = {'finance':  re.compile(r"\b(rent|loan|debt|money|bills?|salary|wages?|paycheck|finance|expensive|cheap|broke|sales?|discount)\b", re.IGNORECASE),
-          'relationships': re.compile(r"\b(girlfriend|boyfriend|husband|wife|partner|spouse|fiance|relationship|dating|ex|cheat(ed)?|valentine)\b", re.IGNORECASE),
+          'relationships': re.compile(r"\b(girlfriend|boyfriend|husband|wife|partner|spouse|fiance|relationship|dating|ex|cheat(ed)?|valentine|bf|gf)\b", re.IGNORECASE),
           'family': re.compile(r"\b(mother|father|mom|dad|parents?|sister|brother|siblings?|child|son|daughter|in[- ]?laws?|grandparents?)\b", re.IGNORECASE),
           'friendship': re.compile(r"\b(friend|bestie|bff|buddy|pal|roommate|flatmate|neighbor)\b", re.IGNORECASE),
           'work': re.compile(r"\b(boss|coworker|colleague|manager|office|job|promotion|workplace|company|employment|employee|employer)\b", re.IGNORECASE),
           'societal norms': re.compile(r"\b(right|wrong|selfish|respect|rude|entitled|ethical|manners|justice|unfair|fair)\b", re.IGNORECASE),
           'education': re.compile(r"\b(school|college|university|classmate|professor|teacher|exam|homework|study|student)\b", re.IGNORECASE),
-          #'emotions': re.compile(r"\b(angry|furious|mad|rage|yell(ed)?|scream(ed)?|argument|fight|rude|disrespect(ed)?)\b", re.IGNORECASE),
           }
 
 corpus = "./data/output.json"
@@ -39,5 +38,5 @@ if __name__ == "__main__":
     print(generate_themes(themes))
 
 ''' This is the result:
-Counter({'societal norms': 61539, 'family': 52716, 'emotions': 47869, 'relationships': 47192, 'finance': 37687, 'friendship': 37528, 'work': 34511, 'education': 31495})
+Counter({'societal norms': 61539, 'family': 52716, 'relationships': 47965, 'finance': 37687, 'friendship': 37528, 'work': 34511, 'education': 31495})
 '''
--- src/topic_modelling.py
+++ src/topic_modelling.py
@@ -70,15 +70,39 @@ if __name__ == "__main__":
     for i, topic in topics:
         print(f"Topic {i + 1}: {topic}")
 
-'''Here is the result:
-Topic 1: 0.016*"wedding" + 0.012*"party" + 0.011*"christmas" + 0.011*"birthday" + 0.010*"plan"
-Topic 2: 0.033*"car" + 0.012*"phone" + 0.011*"drive" + 0.008*"minute" + 0.007*"call"
-Topic 3: 0.012*"room" + 0.009*"help" + 0.007*"clean" + 0.007*"need" + 0.006*"sleep"
-Topic 4: 0.008*"talk" + 0.006*"tell" + 0.006*"talking" + 0.006*"made" + 0.005*"call"
-Topic 5: 0.011*"school" + 0.011*"job" + 0.008*"class" + 0.006*"working" + 0.006*"teacher"
-Topic 6: 0.027*"food" + 0.018*"eat" + 0.013*"dinner" + 0.009*"eating" + 0.008*"drink"
-Topic 7: 0.033*"dog" + 0.012*"room" + 0.011*"cat" + 0.008*"door" + 0.006*"play"
-Topic 8: 0.031*"money" + 0.025*"pay" + 0.011*"job" + 0.009*"rent" + 0.008*"move"
-Topic 9: 0.026*"gift" + 0.016*"christmas" + 0.009*"something" + 0.007*"wanted" + 0.007*"present"
-Topic 10: 0.021*"kid" + 0.016*"husband" + 0.015*"mother" + 0.013*"daughter" + 0.013*"wife"
-'''
+    '''Here is the result:
+    Topic 1: 0.016*"wedding" + 0.012*"party" + 0.011*"christmas" + 0.011*"birthday" + 0.010*"plan"
+    Topic 2: 0.033*"car" + 0.012*"phone" + 0.011*"drive" + 0.008*"minute" + 0.007*"call"
+    Topic 3: 0.012*"room" + 0.009*"help" + 0.007*"clean" + 0.007*"need" + 0.006*"sleep"
+    Topic 4: 0.008*"talk" + 0.006*"tell" + 0.006*"talking" + 0.006*"made" + 0.005*"call"
+    Topic 5: 0.011*"school" + 0.011*"job" + 0.008*"class" + 0.006*"working" + 0.006*"teacher"
+    Topic 6: 0.027*"food" + 0.018*"eat" + 0.013*"dinner" + 0.009*"eating" + 0.008*"drink"
+    Topic 7: 0.033*"dog" + 0.012*"room" + 0.011*"cat" + 0.008*"door" + 0.006*"play"
+    Topic 8: 0.031*"money" + 0.025*"pay" + 0.011*"job" + 0.009*"rent" + 0.008*"move"
+    Topic 9: 0.026*"gift" + 0.016*"christmas" + 0.009*"something" + 0.007*"wanted" + 0.007*"present"
+    Topic 10: 0.021*"kid" + 0.016*"husband" + 0.015*"mother" + 0.013*"daughter" + 0.013*"wife"
+    '''
+
+    topic_counts = Counter()
+
+    for bow in doc_term_matrix:
+        topic_probabilities = lda_model.get_document_topics(bow) # Gets a list of tuples with first index of tuple being the index of topic and second being the probability
+        if topic_probabilities:
+            dominant_topic = max(topic_probabilities, key=lambda x: x[1])[0]
+            topic_counts[dominant_topic] += 1
+    
+    print(topic_counts)
+
+    '''Here is the result:
+    Counter({3: 15432, 9: 9107, 2: 8168, 0: 8014, 7: 6499, 4: 5127, 6: 4575, 1: 3985, 8: 3257, 5: 3127})
+    The indices are shifted by one thus we can say that ->  1) Topic 4: Communication
+                                                            2) Topic 10: Family
+                                                            3) Topic 3: Personal space
+                                                            4) Topic 1: Social events
+                                                            5) Topic 8: Finances
+                                                            6) Topic 5: Education
+                                                            7) Topic 7: Pets
+                                                            8) Topic 2: Transport
+                                                            9) Topic 9: Gifts
+                                                            10) Topic 6: Food
+    '''
